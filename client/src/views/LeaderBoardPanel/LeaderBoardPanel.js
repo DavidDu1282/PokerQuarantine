10f076e0677a5ee85d5853901f548ca6cefd75c2
Marian <mariannguyeen@hotmail.com>
2020-07-23 19:38:14 -0700

    added leaderboard

--- client/src/views/LeaderBoardPanel/LeaderBoardPanel.js
+++ client/src/views/LeaderBoardPanel/LeaderBoardPanel.js
@@ -1,92 +1,87 @@
-import React from "react";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Grid from "@material-ui/core/Grid";
-import { Typography } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 // import tileData from './tileData';
-import axios from "axios";
+import axios from 'axios';
 // import tileData from './tileData';
 
-class ManagementPanel extends React.Component {
+class LeaderBoardPanel extends React.Component {
   state = {
-    title: "",
-    body: "",
-    posts: [],
-  };
+    title: '',
+    body: '',
+    posts: []
+  }
   componentDidMount = () => {
     this.getNewsPost();
   };
 
   getNewsPost = () => {
-    axios
-      .get("/api/top10")
-      .then((response) => {
-        const data = response.data;
-        this.setState({ posts: data });
-        console.log("Data has been received!!");
-      })
-      .catch(() => {
-        alert("Error retrieving data!!!");
-      });
-  };
+    axios.get('/api/top10')
+    .then((response) => {
+      const data = response.data;
+      this.setState({ posts: data });
+      console.log('Data has been received!!');
+    })
+    .catch(() => {
+      alert('Error retrieving data!!!');
+    });
+}
 
-  displayNewsPost = (posts) => {
-    if (!posts.length) return null;
-    return posts.map((post, _id) => (
-      <TableRow key={post._id}>
-        <TableCell>{post.name}</TableCell>
-        <TableCell>{post.defendant}</TableCell>
-        <TableCell>{post.type}</TableCell>
-        <TableCell>{post.info}</TableCell>
-        <TableCell>{post.date.day}</TableCell>
-      </TableRow>
-    ));
-  };
-  render() {
-    console.log("State: ", this.state);
+
+displayNewsPost = (posts) => {
+
+  if (!posts.length) return null;
+  return posts.map((post, _id) => (
+    
+    <TableRow key={post._id}>
+                  <TableCell align = "left">{post.name}</TableCell>
+                  <TableCell align = "center" >{post.games_played}</TableCell>
+                  <TableCell align = "center" >{post.wins}</TableCell>
+                  <TableCell align = "center">{post.losses}</TableCell>
+                  <TableCell align = "center">{post.balance}</TableCell>
+                  
+                </TableRow>
+  ));
+};
+  render(){
+    console.log('State: ', this.state);
     return (
       <div className="container-padded">
-        <Grid
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="flex-start"
-          style={{ flexWrap: "nowrap" }}
-          spacing={4}
-        >
-          <Grid item>
-            <Typography variant="h4">Management</Typography>
-          </Grid>
-          <Table aria-label="simple table">
-            <TableHead>
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="flex-start"
+        style={{flexWrap: "nowrap"}}
+        spacing={4}
+      >
+        <Grid item>
+          <Typography variant="h4">Leaderboard</Typography>
+        </Grid>
+        <Table aria-label="simple table">
+          <TableHead>
               <TableRow>
-                <TableCell>
-                  <b>Reporter</b>
-                </TableCell>
-                <TableCell>
-                  <b>Defendant</b>
-                </TableCell>
-                <TableCell>
-                  <b>Report Type</b>
-                </TableCell>
-                <TableCell>
-                  <b>Details</b>
-                </TableCell>
-                <TableCell>
-                  <b>Date of Report</b>
-                </TableCell>
+                <TableCell align = "left"><b>Username</b></TableCell>
+                <TableCell align = "center"><b>Games played</b></TableCell>
+                <TableCell align = "center"><b>Wins</b></TableCell>
+                <TableCell align = "center"><b>Losses</b></TableCell>
+                <TableCell align = "center"><b>Number of Chips</b></TableCell>
+
               </TableRow>
             </TableHead>
-            {this.displayNewsPost(this.state.posts)}
+          {this.displayNewsPost(this.state.posts)}
           </Table>
-        </Grid>
-      </div>
+          </Grid>
+        </div>
     );
   }
 }
 
-export default ManagementPanel;
+export default LeaderBoardPanel
+
