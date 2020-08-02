4f37127daa05ec07f0ba96c71341bc0280f33580
Vincent Law <31938527+vincentlaw94@users.noreply.github.com>
2020-08-01 21:58:07 -0700

    Deck Object Added

---
+++ game/Deck.js
@@ -0,0 +1,54 @@
+const _ = require("lodash");
+class Deck {
+  constructor() {
+    this.deck = [];
+  }
+  generateDeck() {
+    var tmpDeck = [];
+    var suit = ["d", "c", "h", "s"];
+    var rank = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A",
+    ];
+    var tmpCard = undefined;
+    for (var i = 0; i < rank.length; i++) {
+      for (var j = 0; j < suit.length; j++) {
+        tmpCard = rank[i] + suit[j];
+
+        this.deck.push(tmpCard);
+      }
+    }
+  }
+  shuffle() {
+    this.deck = _.shuffle(this.deck);
+  }
+  drawCard() {
+    let card = this.deck.shift();
+    return card;
+  }
+}
+
+// deck = new Deck();
+// deck.generateDeck();
+// console.log(deck);
+// var draw = deck.drawCard();
+// console.log(draw);
+// console.log(deck.deck.length);
+
+// deck.shuffle();
+// var draw1 = deck.drawCard();
+// console.log(draw1);
+// console.log(deck.deck);
+
+module.exports = Deck;
