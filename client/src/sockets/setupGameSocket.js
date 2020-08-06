6beb7c33ecee105927a4b00003e7f81f00fb4687
Vincent Law <31938527+vincentlaw94@users.noreply.github.com>
2020-08-06 14:17:20 -0700

    fixed parsing error

--- client/src/sockets/setupGameSocket.js
+++ client/src/sockets/setupGameSocket.js
@@ -19,61 +19,34 @@ export default function setupGameSocket(socket, client) {
     }, 1000);
   });
 
-
   socket.on("game_terminate", (reason) => {
     if (client.lobby.current) client.lobby.current.reset();
     client.windowController.current.hide("Lobby");
 
     if (reason && client.in_game) alert(reason);
+  });
 
-  })
-
-  socket.on("get_table",(tableData)=>{
-    if(process.env.NODE_ENV === "development"){
+  socket.on("get_table", (tableData) => {
+    if (process.env.NODE_ENV === "development") {
       console.log("Received get_table!");
     }
 
     client.TexasHoldemGamePage.get_table(tableData);
-
-  })
-  socket.on('get_current_status',(updateData)=>{
-      if(process.env.NODE_ENV === "development"){
-        console.log("Received get_game_status!");
-      }
-      var newBet = 0;
-      if(client.TexasHoldemGamePage.state.round === updateData.round){
-        newBet = updateData.pot - this.client.TexasHoldemGamePage.state.potTotal;
-      }
-      else{
-        client.TexasHoldemGamePage.setState(((state) => {return {round: updateData.round}}));
-      }
-      client.TexasHoldemGamePage.setState(((state) => {return {
-        lastPlayerBetAmount: newBet,
-        turnPosition: updateData.turnPos
-      }}));
-      if(updateData.turnPos === client.TexasHoldemGamePage.state.playerPosition){
-        client.TexasHoldemGamePage.handleOpen();
-      }
-      /*
-      this.client.TexasHoldemGamePage.state.LeftTable.map(elem => (
-        if(elem.playerID = updateDataplayerID)
-      ))
-      */
-  })
-  socket.on('get_game_status',(updateData)=>{
-      if(process.env.NODE_ENV === "development"){
-        console.log("Received get_game_status!");
-      }
-      var newBet = 0;
-      if(client.TexasHoldemGamePage.state.round === updateData.round){
-        newBet = updateData.pot - this.client.TexasHoldemGamePage.state.potTotal;
-      }
-      else{
-        client.TexasHoldemGamePage.setState(((state) => {return {round: updateData.round}}));
-
-      }
-      client.TexasHoldemGamePage.setState(((state) => {return {
-
+  });
+  socket.on("get_current_status", (updateData) => {
+    if (process.env.NODE_ENV === "development") {
+      console.log("Received get_game_status!");
+    }
+    var newBet = 0;
+    if (client.TexasHoldemGamePage.state.round === updateData.round) {
+      newBet = updateData.pot - this.client.TexasHoldemGamePage.state.potTotal;
+    } else {
+      client.TexasHoldemGamePage.setState((state) => {
+        return { round: updateData.round };
+      });
+    }
+    client.TexasHoldemGamePage.setState((state) => {
+      return {
         lastPlayerBetAmount: newBet,
         turnPosition: updateData.turnPos,
       };
@@ -88,11 +61,38 @@ export default function setupGameSocket(socket, client) {
         if(elem.playerID = updateDataplayerID)
       ))
       */
-
-  })
-  socket.on('endOfGame',(playerInfo)=>{
-    if(process.env.NODE_ENV === "development"){
-
+  });
+  socket.on("get_game_status", (updateData) => {
+    if (process.env.NODE_ENV === "development") {
+      console.log("Received get_game_status!");
+    }
+    var newBet = 0;
+    if (client.TexasHoldemGamePage.state.round === updateData.round) {
+      newBet = updateData.pot - this.client.TexasHoldemGamePage.state.potTotal;
+    } else {
+      client.TexasHoldemGamePage.setState((state) => {
+        return { round: updateData.round };
+      });
+    }
+    client.TexasHoldemGamePage.setState((state) => {
+      return {
+        lastPlayerBetAmount: newBet,
+        turnPosition: updateData.turnPos,
+      };
+    });
+    if (
+      updateData.turnPos === client.TexasHoldemGamePage.state.playerPosition
+    ) {
+      client.TexasHoldemGamePage.handleOpen();
+    }
+    /*
+      this.client.TexasHoldemGamePage.state.LeftTable.map(elem => (
+        if(elem.playerID = updateDataplayerID)
+      ))
+      */
+  });
+  socket.on("endOfGame", (playerInfo) => {
+    if (process.env.NODE_ENV === "development") {
       console.log("Received endOfGame!");
     }
     if (client.user.id === playerInfo.winner) {
